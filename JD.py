0f6983b34fb17a477e1fe4e6a5fe3841297951ec
vildandizdar <84573331+vildandizdar@users.noreply.github.com>
2023-05-21 12:36:20 +0300

    JD.py

--- JD.py
+++ JD.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 # Modülleri yüklüyoruz.
 from datetime import date
 
@@ -11,24 +10,13 @@ Gün = int(input("Gün giriniz."))
 Ay = int(input("Ay giriniz.")) 
 Yıl = int(input("Yıl giriniz.")) 
 
-
 dt1 = date(year = Yıl, month = Ay, day = Gün)
-
 dt2 = date(year = Yıl, month = 1, day = 1)
-
 dt3 = (dt1 - dt2).days
 dt3 = dt3+1
-
 
 N = dt3
 L = int((Yıl-1901)/4)
 
-
-
-
 JD = 2415020 + 365*(Yıl-1900) + N + L - 0.5
 print (dt1, "Tarihinin Julian Günü:", JD)
-
-
-
-
